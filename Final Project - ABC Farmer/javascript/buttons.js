21d1ebec398a7b97ffb5181bf9f90afe07323ec1
mukasama <mukasama@msu.edu>
2014-12-10 21:12:38 -0500

    Final Project - ABC Farmer
    Final Project - ABC Farmer

---
+++ Final Project - ABC Farmer/javascript/buttons.js
@@ -0,0 +1,15 @@
+function OnPress(buttonPressed) 
+{
+	
+if (buttonPressed == 'Funding') {
+	window.location='http://www.acdi-cida.gc.ca/CIDAWEB/cpo.nsf/fWebCSAZEn?ReadForm&idx=00&CC=UG';
+}
+else if (buttonPressed == 'Videos') 
+{
+	window.location='https://www.farmafrica.org/us/videos/videos';
+}
+else if (buttonPressed == 'Links')
+{
+	window.location='https://www.google.com/search?espv=2&biw=1037&bih=557&q=uganda+agriculture&oq=Ugandan+Agri&gs_l=serp.3.0.0i10l10.25509.34361.0.35385.24.18.6.0.0.0.343.1767.1j10j0j1.12.0.chm_loc...0...1c.1.60.serp..7.17.1594.r9RJaUz4Qs4';
+}
+}
